03bcb7cc73cff33af2d34a923b00fe9f1f8e6341
Med <mohamed.mehmli@digit-u.com>
2018-06-03 17:51:59 +0200

    Implement basic logic of mapping the users to their socket IDs.

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const PORT = process.env.PORT || 3000;
 
 require('./init'); // Init MongoDB connection
 
+const USERS_CHATS = {};
+
 const Message = require('./models/message');
 
 io.on('connection', socket => {
+  socket.on('user_connected', async (content, fn) => {
+    USERS_CHATS[content.userId] = socket.id;
+  });
+
   socket.on('message_sent', async (content, fn) => {
+    USERS_CHATS[content.sender] = socket.id;
+
     const msg = new Message(content);
 
     try {
@@ -20,6 +28,18 @@ io.on('connection', socket => {
           content.receiver
         } saved under _id ${result._id}`
       );
+
+      // Send back the full message object saved in the DB.
+      fn(result);
+
+      const receiverSocketId = USERS_CHATS[content.receiver];
+
+      if (receiverSocketId) {
+        io.sockets.connected[receiverSocketId].emit(
+          'message_received',
+          content.message
+        );
+      }
     } catch (err) {
       console.log(err);
     }
